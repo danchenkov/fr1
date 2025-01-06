d177a06dbf355e0c960f98167a7de039ce53380e
Alexei Danchenkov <adanchenkov@emailline.net>
2025-01-06 12:20:20 +0300

    some exercizes

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,51 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { createElement } from 'react'
+import { createRoot } from "react-dom/client"
+const root = createRoot(document.getElementById("root"))
 
-const root = createRoot(document.getElementById('root'))
-const reactElement = createElement(<h1>Hello, <span>world</span>!</h1>)
+function Header() {
+    return (
+        <header className="header">
+            <img src="/src/assets/react.svg" alt="React logo" />
+			<nav className="top-nav">
+				<ul className="nav-list">
+					<li>Pricing</li>
+					<li>About</li>
+					<li>Contact</li>
+				</ul>
+			</nav>
+        </header>
+    )
+}
 
-root.render(reactElement)
+function MainContent() {
+    return (
+        <main>
+            <h1>Reason I am excited to learn React</h1>
+            <ol>
+                <li>React is a popular library, so I will be able to fit in with all the coolest devs out there! 😎</li>
+                <li>I am more likely to get a job as a front end developer if I know React</li>
+            </ol>
+        </main>
+    )
+}
+
+function Footer() {
+    return (
+        <footer>
+            <small>© 2024 Ziroll development. All rights reserved.</small>
+        </footer>
+    )
+}
+
+function Page() {
+    return (
+        <>
+            <Header />
+            <MainContent />
+            <Footer />
+        </>
+    )
+}
+
+root.render(
+    <Page />
+)
