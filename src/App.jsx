d42916e29de2a465e66d95dafd0148971e0a0cd5
Alexei Danchenkov <adanchenkov@emailline.net>
2025-01-08 00:53:29 +0300

    mapping data

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,40 @@
-import Header from "./components/Header"
-import Entry from "./components/Entry"
-
-/**
- * Challenge: pass props to the Entry component to display
- * its data. See the `data.md` file for each prop name and its
- * value.
- *
- * Then on the Entry component, receive and display the values
- * for those props. In the end, the page should look the same
- * as it does now, but without all the hard-coded data in the
- * component
- */
+import Joke from "/src/components/Joke"
+import jokesData from "/src/jokesData"
 
 export default function App() {
+
+    /**
+     * Challenge: See if you can correctly pass the necessary props to the
+     * Joke component in the .map() (and render the jokeElements array) so
+     * the jokes show up on the page again
+     */
+
+    const jokeElements = jokesData.map((joke) => {
+        return <Joke setup={joke.setup} punchline={joke.punchline} />
+    })
     return (
-        <>
-            <Header />
-            <main className="container">
-                <Entry
-					title="Mount Fuji"
-					img={{
-						src: "https://scrimba.com/links/travel-journal-japan-image-url",alt: "Mount Fuji"
-					}}
-					country="Japan"
-					googleMapsLink="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu"
-					dates="12 Jan, 2021 - 24 Jan, 2021"
-					text="Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
-				/>
-            </main>
-        </>
+        <main>
+			{jokeElements}
+        </main>
     )
 }
+
+// <Joke
+//     punchline="It's hard to explain puns to kleptomaniacs because they always take things literally."
+// />
+// <Joke
+//     setup="How did the hacker escape the police?"
+//     punchline="He just ransomware!"
+// />
+// <Joke
+//     setup="Why don't pirates travel on mountain roads?"
+//     punchline="Scurvy."
+// />
+// <Joke
+//     setup="Why do bees stay in the hive in the winter?"
+//     punchline="Swarm."
+// />
+// <Joke
+//     setup="What's the best thing about Switzerland?"
+//     punchline="I don't know, but the flag is a big plus!"
+// />
