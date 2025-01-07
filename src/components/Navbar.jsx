695faf594f59acde2d4f13ace4e80b046d5107b3
Alexei Danchenkov <adanchenkov@emailline.net>
2025-01-07 11:39:26 +0300

    assignment #1 with styling

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,8 @@
+export default function Navbar() {
+    return (
+        <header className="header-main">
+            <img src="/src/assets/react.svg" alt="React logo" />
+            <span>ReactFacts</span>
+        </header>
+    )
+}
--- src/footer.jsx
+++
@@ -1,8 +0,0 @@
-export default function Footer() {
-    return (
-        <footer className="footer-main">
-            <small>© 2025 Alexei Danchenkov. All rights reserved.</small>
-        </footer>
-    )
-}
-
