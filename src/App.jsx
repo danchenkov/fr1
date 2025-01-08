06fa3a6637f43189bcebe55da7b30af3c6938719
Alexei Danchenkov <adanchenkov@emailline.net>
2025-01-08 20:54:31 +0300

    Chef Claude beginning

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,24 @@
-import Joke from "/src/components/Joke"
-import jokesData from "/src/jokesData"
+import Header from "./components/Header"
+import Entry from "./components/Entry"
+
+import entryData from "./data"
+
+/**
+ * Challenge:
+ * - import the array of data from data.js
+ * - map over the array to create an <Entry /> component
+ *   for every item in the data array.
+ * - display the array of Entry components in place of the current
+ *   hard-coded <Entry /> instance.
+ */
 
 export default function App() {
-
-    /**
-     * Challenge: See if you can correctly pass the necessary props to the
-     * Joke component in the .map() (and render the jokeElements array) so
-     * the jokes show up on the page again
-     */
-
-    const jokeElements = jokesData.map((joke) => {
-        return <Joke setup={joke.setup} punchline={joke.punchline} />
-    })
     return (
-        <main>
-			{jokeElements}
-        </main>
+        <>
+            <Header />
+            <main className="container">
+				{entryData.map(entry => <Entry key={entry.id} {...entry} />)}
+            </main>
+        </>
     )
 }
-
-// <Joke
-//     punchline="It's hard to explain puns to kleptomaniacs because they always take things literally."
-// />
-// <Joke
-//     setup="How did the hacker escape the police?"
-//     punchline="He just ransomware!"
-// />
-// <Joke
-//     setup="Why don't pirates travel on mountain roads?"
-//     punchline="Scurvy."
-// />
-// <Joke
-//     setup="Why do bees stay in the hive in the winter?"
-//     punchline="Swarm."
-// />
-// <Joke
-//     setup="What's the best thing about Switzerland?"
-//     punchline="I don't know, but the flag is a big plus!"
-// />
