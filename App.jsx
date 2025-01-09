34a9234c736f10d546e72bc20bc21bd62ca35479
Alexei Danchenkov <adanchenkov@emailline.net>
2025-01-09 18:00:24 +0300

    adding ingredients

--- App.jsx
+++ App.jsx
@@ -1,10 +1,11 @@
-/**
- * Challenge: Build the Header component in a separate file
- * and render it here in the App component
- */
+import Header from './Header'
+import Main from './Main'
 
 export default function App() {
-  return (
-      <h1>Hello world!</h1>
-  )
+    return (
+        <>
+            <Header />
+            <Main />
+        </>
+    )
 }
---
+++ Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+	return (
+		<header>
+			<img src="./images/chef-claude-icon.png" alt="Chef Claude Icon" />
+			<h1>Chef Claude</h1>
+		</header>
+	)
+}
